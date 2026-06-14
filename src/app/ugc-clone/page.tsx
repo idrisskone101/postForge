c0d44b03c75508c6de441c574bcb233df70182c8
Idriss Kone <99677465+idrisskone101@users.noreply.github.com>
2026-06-14 12:33:17 -0400

    Remove clone activity from clone workspace

--- src/app/ugc-clone/page.tsx
+++ src/app/ugc-clone/page.tsx
@@ -1,15 +1,10 @@
 import { UGCCloneForm } from "@/components/ugc-clone-form";
-import { UGCCloneQueue } from "@/components/ugc-clone-queue";
 
 export default function UGCClonePage() {
   return (
     <div className="min-h-[calc(100vh-76px)] overflow-x-hidden bg-[oklch(0.145_0_0)]">
       <div className="mx-auto min-w-0 max-w-6xl px-4 py-6 sm:px-6 lg:px-8 lg:py-8">
         <UGCCloneForm />
-
-        <div className="mt-8">
-          <UGCCloneQueue />
-        </div>
       </div>
     </div>
   );
